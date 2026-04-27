8195bc94ae61f6cd31ad331171b57590c7620651
Deezay10 <nino.caillaud@gmail.com>
2026-04-27 11:57:50 +0200

    Ajout page Map + route, Change html connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Accueil } from './pages/accueil/accueil';
 import { Connexion } from './pages/connexion/connexion';
 import { Inventaire } from './pages/inventaire/inventaire';
 import { Inscription } from './pages/Inscription/inscription';
+import { Map } from './pages/map/map';
 
 export const routes: Routes = [
   { path: 'home', component: Accueil },
   { path: 'inscription', component: Inscription },
   { path: 'connexion', component: Connexion },
   { path: 'inventaire', component: Inventaire },
+  {path: 'map', component: Map },
 ];
---
+++ src/app/pages/map/map.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-map',
+  //imports: [NgStyle, RouterLink],
+  templateUrl: './map.html',
+  styleUrl: './map.scss',
+})
+export class Map {
+}

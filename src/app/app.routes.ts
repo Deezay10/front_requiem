37601b19012aeff28dd4330c93c6cdd9b1b284df
Lucas MOLENDI <lucasmolendi0@gmail.com>
2026-04-27 11:50:26 +0200

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Accueil } from './pages/accueil/accueil';
 import { Connexion } from './pages/connexion/connexion';
 import { Inventaire } from './pages/inventaire/inventaire';
+import { Inscription } from './pages/Inscription/inscription';
 
 export const routes: Routes = [
   { path: 'home', component: Accueil },

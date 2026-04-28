c0e27884625f8d9748169b99873899ee28aeebad
Deezay10 <nino.caillaud@gmail.com>
2026-04-28 16:21:59 +0200

    connexion ez

--- src/app/pages/connexion/connexion.ts
+++ src/app/pages/connexion/connexion.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-connexion',
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule],
   templateUrl: './connexion.html',
   styleUrl: './connexion.scss',
 })
 export class Connexion {
   email: string = '';
-  password: string = '';
+  motdepasse: string = '';
   message: string = '';
 
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {}
+
+  login() {
+    const data = { email: this.email, motdepasse: this.motdepasse };
+
+    this.http.post('http://localhost:8080/users/login', data).subscribe({
+      next: (response) => {
+        this.message = 'Connexion réussie !';
+        this.router.navigate(['/inventaire']);
+      },
+      error: (err) => {
+        this.message = 'Email ou mot de passe incorrect';
+      },
+    });
   }
+}

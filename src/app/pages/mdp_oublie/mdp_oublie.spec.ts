7130874942c3dd6a7d59e08810e91e9ebed82909
Lucas MOLENDI <lucasmolendi0@gmail.com>
2026-04-28 12:02:24 +0200

    mot de passe oubliée function

---
+++ src/app/pages/mdp_oublie/mdp_oublie.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MdpOublie } from './mdp_oublie';
+
+describe('MdpOublie', () => {
+  let component: MdpOublie;
+  let fixture: ComponentFixture<MdpOublie>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MdpOublie],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MdpOublie);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

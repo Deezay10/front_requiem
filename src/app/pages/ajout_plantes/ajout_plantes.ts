9a9d49e52e48ffaf653116f0b80246516865379a
Deezay10 <nino.caillaud@gmail.com>
2026-04-30 11:58:42 +0200

    Inventaire avec superficie et date + ajout_plantes fonctionnel

--- src/app/pages/ajout_plantes/ajout_plantes.ts
+++ src/app/pages/ajout_plantes/ajout_plantes.ts
@@ -1,13 +1,58 @@
 import { Component } from '@angular/core';
-import { NgStyle } from '@angular/common';
-import { RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ServicesConnexion } from '../../services/services-connexion';
 
 @Component({
-  selector: 'app-accueil',
-  //imports: [NgStyle, RouterLink],
+  selector: 'app-ajout-plantes',
+  imports: [FormsModule, CommonModule],
   templateUrl: './ajout_plantes.html',
-  styleUrl: './ajout_plantes.scss',
+  styleUrls: ['./ajout_plantes.scss'],
 })
 export class AjoutPlantes {
+
+  plantation = {
+    nom: "",
+    surface_m2: 0,
+  };
+
+  public legumes: any[] = [];
+  public message = "";
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private servicesConnexion: ServicesConnexion
+  ) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('http://localhost:8080/legumes').subscribe({
+      next: (legumes) => {
+        this.legumes = legumes;
+      },
+      error: (err) => {
+        console.error('Erreur récupération légumes : ', err);
+      }
+    });
+  }
+
+  add() {
+    const user = this.servicesConnexion.getUser();
+
+    if (!user) {
+      this.message = "Vous devez être connecté";
+      return;
+    }
+
+    this.http.post(`http://localhost:8080/add_plantation/users/${user.id}`, this.plantation).subscribe({
+      next: (response) => {
+        this.router.navigate(['/inventaire']);
+      },
+      error: (err) => {
+        this.message = "Plante non trouvée ou erreur lors de l'ajout";
+      }
+    });
+  }
 }

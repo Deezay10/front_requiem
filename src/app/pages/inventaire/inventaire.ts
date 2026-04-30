3628949a768514516efe0dcb50349e442a8d7cac
Deezay10 <nino.caillaud@gmail.com>
2026-04-30 09:36:53 +0200

    Inventaire finito

--- src/app/pages/inventaire/inventaire.ts
+++ src/app/pages/inventaire/inventaire.ts
@@ -5,6 +5,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { routes} from '../../app.routes';
 import {Legume} from '../../services/services-inventaire';
 import { ServicesConnexion } from '../../services/services-connexion';
+import { ChangeDetectorRef } from '@angular/core';
 
 declare var UIkit: any;
 
@@ -24,7 +25,8 @@ export class Inventaire implements OnInit {
   constructor(
     private servicesConnexion: ServicesConnexion,
     private http: HttpClient,
-    private ngZone: NgZone
+    private ngZone: NgZone,
+    private cd:ChangeDetectorRef
   ) {}
 
   ngOnInit() {
@@ -32,31 +34,27 @@ export class Inventaire implements OnInit {
     console.log("User complet : ", this.user);
     console.log("User id : ", this.user.id);
 
-    // Test temporaire
-    this.legumes = [{ id: "test", nom: "Tomate test", image_url: "https://images.unsplash.com/photo-1546094096-0df4bcaaa337?w=300", croissance_jours: 70, rendement: 6, saisons: ["printemps"] }];
-    this.liste_saison["test"] = "printemps";
-
     if (this.user) {
-        this.http.get<any[]>(`http://localhost:8080/inventaire/users/${this.user.id}`).subscribe({
-          next: (legumes) => {
-            this.ngZone.run(() => {
-            console.log("Légumes reçus : ", legumes);
-            this.legumes = legumes;
-            for (let legume of this.legumes) {
-              if (legume.saisons && legume.saisons.length > 1) {
-                this.liste_saison[legume.id] = legume.saisons.join(', ');
-              } else if (legume.saisons && legume.saisons.length === 1) {
-                this.liste_saison[legume.id] = legume.saisons[0];
-              } else {
-                this.liste_saison[legume.id] = 'Non renseigné';
-              }
+      this.http.get<any[]>(`http://localhost:8080/inventaire/users/${this.user.id}`).subscribe({
+        next: (legumes) => {
+          console.log("Légumes reçus : ", legumes);
+          this.legumes = legumes;
+
+          for (let legume of this.legumes) {
+            if (legume.saisons && legume.saisons.length > 1) {
+              this.liste_saison[legume.id] = legume.saisons.join(', ');
+            } else if (legume.saisons && legume.saisons.length === 1) {
+              this.liste_saison[legume.id] = legume.saisons[0];
+            } else {
+              this.liste_saison[legume.id] = 'Non renseigné';
             }
-            });
-          },
-          error: (err) => {
+          }
+          this.cd.detectChanges();
+        },
+        error: (err) => {
           console.error('Erreur récupération légumes : ', err);
-          },
-        });
+        }
+      });
     }
   }
 }

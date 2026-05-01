eb5a905ccacedf25d4d6d79f3257d82febb62694
Lucas MOLENDI <lucasmolendi0@gmail.com>
2026-05-01 12:41:01 +0200

    dashboard

--- src/app/pages/accueil/accueil.ts
+++ src/app/pages/accueil/accueil.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { NgStyle } from '@angular/common';
+import { ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
+import { CommonModule, NgStyle } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
@@ -7,27 +7,56 @@ import { ServicesConnexion } from '../../services/services-connexion';
 
 @Component({
   selector: 'app-accueil',
-  //imports: [NgStyle, RouterLink],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './accueil.html',
   styleUrl: './accueil.scss',
 })
-export class Accueil {
+
+export class Accueil implements OnInit {
   public link: SafeResourceUrl = '';
+  public legumes: any[] = [];
+  public liste_saison: { [key: string]: string } = {};
 
   constructor(
     private sanitizer: DomSanitizer,
     private servicesConnexion: ServicesConnexion,
+    private http: HttpClient,
+    private ngZone: NgZone,
     private cd: ChangeDetectorRef,
   ) {}
 
   ngOnInit() {
     const user = this.servicesConnexion.getUser();
-    if (user && user.adresse) {
+
+    if (user?.adresse) {
       const adresseFormatee = user.adresse.replaceAll(' ', '+');
       this.link = this.sanitizer.bypassSecurityTrustResourceUrl(
         'https://maps.google.com/maps?q=' + adresseFormatee + '&t=k&output=embed',
       );
-      this.cd.detectChanges();
+    }
+
+    if (user?.id) {
+      this.http.get<any[]>(`http://localhost:8080/inventaire/users/${user.id}`).subscribe({
+        next: (legumes) => {
+          this.legumes = legumes;
+
+          for (let legume of this.legumes) {
+            if (legume.saisons?.length > 1) {
+              this.liste_saison[legume.id] = legume.saisons.join(', ');
+            } else if (legume.saisons?.length === 1) {
+              this.liste_saison[legume.id] = legume.saisons[0];
+            } else {
+              this.liste_saison[legume.id] = 'Non renseigné';
+            }
+          }
+
+          this.cd.detectChanges();
+        },
+        error: (err) => {
+          console.error('Erreur récupération légumes : ', err);
+        },
+      });
     }
   }
 }

cbbfb62c3892c20fd8baf5fec9927c19baeb2572
Deezay10 <nino.caillaud@gmail.com>
2026-05-04 09:35:03 +0200

    app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimations} from '@angular/platform-browser/animations';
+import { authInterceptor} from './services/auth.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     //provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
     //provideAnimations()
   ]
 };
